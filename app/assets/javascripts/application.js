6b979e7919549e0339857621ddf06bb27fc8fc7e
Arif Liya <arif@Arifs-MacBook-Pro.local>
2021-08-02 09:21:40 +0100

    all files

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,17 @@
+/* global $ */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+});
+
+
+
+$('#backLink').click(function (event) {
+  event.preventDefault()
+  history.back(1)
+});
