7beded32ac8da1445880449482ed19f43865cb1e
Arif Liya <arif@Arifs-MacBook-Pro.local>
2021-08-09 10:55:05 +0100

    content updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,24 @@ $(document).ready(function () {
 });
 
 
-
+// Back button code
 $('#backLink').click(function (event) {
   event.preventDefault()
   history.back(1)
 });
+//********************** */
+
+
+// Adding numbers on gov-04.html
+var numberOne = parseInt($('#staffGovernors').html());
+var numberTwo = parseInt($('#studentGovernors').html());
+var numberThree = parseInt($('#independantGovernors').html());
+var numberFour = parseInt($('#parentGovernors').html());
+
+var total = numberOne + numberTwo + numberThree + numberFour
+
+$('#totalGovernors').text('');
+$('#totalGovernors').append(total);
+//*********************** */
+
+
